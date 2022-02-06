87ea7a8e2ad3cb33aec59a242b66d1a6deb67895
kevster01 <90598902+kevster01@users.noreply.github.com>
2022-02-06 13:22:32 -0800

    Add files via upload
    source code for finding words ending with letter e or t with images of code working.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Program 
+{
+
+
+    public class Kevin
+    {
+    
+    
+    
+         public static void Main()
+            {
+
+            int wrd, l;
+
+      
+            string  str = System.IO.File.ReadAllText("text3.txt");
+       
+
+            l = 1;
+            wrd = 1;
+
+    
+             while (l <= str.Length - 1)
+             { 
+    
+       
+                if((str[l]=='.' || str[l]== ' ' || str[l]=='\t' || str[l] == '!' || str[l] == ','||str[l] == '?'|| str[l] == '\n') && (str[l-1] == 't' || str[l-1] == 'e'))
+                {
+                    wrd++;
+                 }
+        
+
+                     l++;
+             }
+
+            Console.Write("There are {0} words ending in t or e", wrd);
+         }
+
+
+    }
+}
